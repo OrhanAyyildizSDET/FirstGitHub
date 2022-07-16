e7ff7f5f5a60e39d42a79e58aa8133b84bf13846
Orhan Ayyıldız <ayyildizorhann03@gmail.com>
2022-07-16 22:47:15 +0300

    my dream

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,6 @@
 public class Car {
     public static void main(String[] args) {
         System.out.println("Aston Martin is the best car for me");
+        System.out.println("And one day ı hope ı will have one of Aston martin");
     }
 }
