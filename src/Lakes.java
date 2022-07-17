1aa9efc37df7dcb863f21ee43735550089cc5f29
WebcoderA-Z <100473852+WebcoderA-Z@users.noreply.github.com>
2022-07-17 19:11:31 +0300

    Update Lakes.java

--- src/Lakes.java
+++ src/Lakes.java
@@ -1,5 +1,6 @@
 public class Lakes {
     public static void main(String[] args) {
         System.out.println("BlackSea is the biggest lake on the planet");
+        System.out.println("Lut is the deepest lake on the planet");
     }
 }
